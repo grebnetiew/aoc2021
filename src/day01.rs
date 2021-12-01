c56c83ce91b9361ba0f9de32c931be4a309b5763
Erik Weitenberg <eweitenberg@gmail.com>
2021-12-01 11:26:27 +0100

    day 1

---
+++ src/day01.rs
@@ -0,0 +1,42 @@
+#[aoc_generator(day1)]
+pub fn one_u32_per_line(input: &str) -> Vec<u32> {
+    input.lines().filter_map(|s| s.parse().ok()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[u32]) -> u32 {
+    input
+        .windows(2)
+        .map(|w| if w[1] > w[0] { 1 } else { 0 })
+        .sum()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[u32]) -> u32 {
+    let windows: Vec<_> = input.windows(3).map(|w| w.iter().sum()).collect();
+    part1(&windows)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample1() {
+        assert_eq!(
+            part1(&one_u32_per_line(
+                "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
+            )),
+            7
+        );
+    }
+    #[test]
+    fn sample2() {
+        assert_eq!(
+            part2(&one_u32_per_line(
+                "199\n200\n208\n210\n200\n207\n240\n269\n260\n263"
+            )),
+            5
+        );
+    }
+}
