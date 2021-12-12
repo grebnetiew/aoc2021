105c96b21c6f766a270e5ee646d4236ec0b8a441
Erik Weitenberg <eweitenberg@gmail.com>
2021-12-12 22:04:36 +0100

    slight refactor

--- src/day12.rs
+++ src/day12.rs
@@ -1,7 +1,7 @@
-use petgraph::{Graph, Undirected};
+type Graph = petgraph::Graph<(), (), petgraph::Undirected, usize>;
 
 #[aoc_generator(day12)]
-pub fn parse_graph_edges(input: &str) -> (Vec<String>, Graph<(), (), Undirected, usize>) {
+pub fn parse_graph_edges(input: &str) -> (Vec<String>, Graph) {
     let mut names = Vec::new();
     let graph = Graph::from_edges(
         input
@@ -13,7 +13,7 @@ pub fn parse_graph_edges(input: &str) -> (Vec<String>, Graph<(), (), Undirected,
 }
 
 #[aoc(day12, part1)]
-pub fn part1(input: &(Vec<String>, Graph<(), (), Undirected, usize>)) -> usize {
+pub fn part1(input: &(Vec<String>, Graph)) -> usize {
     // How many parts from the node named start to the node named end, noting
     // that a path may not visit a node with a lower case name more than once?
     let start = find(&input.0, "start").unwrap();
@@ -21,7 +21,7 @@ pub fn part1(input: &(Vec<String>, Graph<(), (), Undirected, usize>)) -> usize {
 }
 
 #[aoc(day12, part2)]
-pub fn part2(input: &(Vec<String>, Graph<(), (), Undirected, usize>)) -> usize {
+pub fn part2(input: &(Vec<String>, Graph)) -> usize {
     // How many parts from the node named start to the node named end, noting
     // that a path may not visit a node with a lower case name more than once,
     // except for one lower case node that may be visited twice?
@@ -55,18 +55,18 @@ fn is_uppercase(s: &str) -> bool {
 /// How many parts from the node named `from` to the node named end, noting
 /// that a path may not visit a node with a lower case name more than once?
 fn depth_first_search_end(
-    graph: &Graph<(), (), Undirected, usize>,
+    graph: &Graph,
     names: &[String],
     visited: &mut Vec<usize>,
     source: usize,
 ) -> usize {
     let mut n_paths = 0;
     visited.push(source);
-    for i in graph.neighbors(source.into()) {
-        if names[i.index()] == "end" {
+    for i in graph.neighbors(source.into()).map(|i| i.index()) {
+        if names[i] == "end" {
             n_paths += 1;
-        } else if is_uppercase(&names[i.index()]) || !visited.contains(&i.index()) {
-            n_paths += depth_first_search_end(graph, names, visited, i.index());
+        } else if is_uppercase(&names[i]) || !visited.contains(&i) {
+            n_paths += depth_first_search_end(graph, names, visited, i);
         }
     }
     assert_eq!(visited.pop(), Some(source));
@@ -77,23 +77,23 @@ fn depth_first_search_end(
 /// that a path may not visit a node with a lower case name more than once,
 /// except for one lower case node that may be visited twice?
 fn depth_first_search_end_exception(
-    graph: &Graph<(), (), Undirected, usize>,
+    graph: &Graph,
     names: &[String],
     visited: &mut Vec<usize>,
     source: usize,
 ) -> usize {
     let mut n_paths = 0;
     visited.push(source);
-    for i in graph.neighbors(source.into()) {
-        if names[i.index()] == "end" {
+    for i in graph.neighbors(source.into()).map(|i| i.index()) {
+        if names[i] == "end" {
             n_paths += 1;
-        } else if is_uppercase(&names[i.index()]) || !visited.contains(&i.index()) {
-            n_paths += depth_first_search_end_exception(graph, names, visited, i.index());
-        } else if names[i.index()] != "start" {
+        } else if is_uppercase(&names[i]) || !visited.contains(&i) {
+            n_paths += depth_first_search_end_exception(graph, names, visited, i);
+        } else if names[i] != "start" {
             // It is lowercase, and it was visited, but we haven't used the Special Exception yet!
             // Use the algo from part 1, which will not make more exceptions, to search for "end"
             // from this point onwards
-            n_paths += depth_first_search_end(graph, names, visited, i.index());
+            n_paths += depth_first_search_end(graph, names, visited, i);
         }
     }
     assert_eq!(visited.pop(), Some(source));
