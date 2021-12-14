bf63d480335039287ca0cfc0ef894edad87e8c86
Erik Weitenberg <eweitenberg@gmail.com>
2021-12-14 13:13:20 +0100

    day 14

---
+++ src/day14.rs
@@ -0,0 +1,123 @@
+use std::collections::HashMap;
+
+pub fn parse_polymer(input: &str) -> (String, HashMap<(u8, u8), u8>) {
+    let mut input = input.lines();
+    let polymer = input.next().unwrap().to_string();
+    let transforms = input
+        .filter(|l| !l.is_empty())
+        .map(|s| {
+            let s = s.as_bytes();
+            ((s[0], s[1]), *s.last().unwrap())
+        })
+        .collect();
+    (polymer, transforms)
+}
+
+#[aoc(day14, part1)]
+pub fn part1(input: &str) -> usize {
+    let (mut polymer, transforms) = parse_polymer(input);
+    for _ in 0..10 {
+        polymer = transform_polymer(polymer, &transforms);
+    }
+
+    // count occurrence of individual letters
+    let mut hist = HashMap::new();
+    for ch in polymer.chars() {
+        *hist.entry(ch).or_insert(0) += 1;
+    }
+
+    let max = *hist.values().max().unwrap();
+    let min = *hist.values().min().unwrap();
+    max - min
+}
+
+fn transform_polymer(polymer: String, transforms: &HashMap<(u8, u8), u8>) -> String {
+    let mut new_polymer = String::new();
+    new_polymer.push(polymer.chars().next().unwrap());
+
+    polymer.as_bytes().windows(2).for_each(|w| {
+        if let Some(b) = transforms.get(&(w[0], w[1])) {
+            new_polymer.push(*b as char);
+        }
+        new_polymer.push(w[1] as char);
+    });
+    new_polymer
+}
+
+#[aoc(day14, part2)]
+pub fn part2(input: &str) -> usize {
+    let (polymer, transforms) = parse_polymer(input);
+    let mut polymer_pairs = HashMap::new();
+    for w in polymer.as_bytes().windows(2) {
+        *polymer_pairs.entry((w[0], w[1])).or_insert(0) += 1;
+    }
+
+    for _ in 0..40 {
+        polymer_pairs = transform_polymer_by_pairs(polymer_pairs, &transforms);
+    }
+
+    // count occurrence of individual letters
+    let mut hist = HashMap::new();
+    for (pair, freq) in polymer_pairs {
+        *hist.entry(pair.0).or_insert(0) += freq;
+        *hist.entry(pair.1).or_insert(0) += freq;
+    }
+
+    // we count everything twice except for the first and last letter
+    let polybytes = polymer.as_bytes();
+    *hist.entry(*polybytes.first().unwrap()).or_insert(0) += 1;
+    *hist.entry(*polybytes.last().unwrap()).or_insert(0) += 1;
+
+    let max = *hist.values().max().unwrap() / 2;
+    let min = *hist.values().min().unwrap() / 2;
+    max - min
+}
+
+fn transform_polymer_by_pairs(
+    polymer: HashMap<(u8, u8), usize>,
+    transforms: &HashMap<(u8, u8), u8>,
+) -> HashMap<(u8, u8), usize> {
+    let mut new_polymer = HashMap::new();
+    for (pair, freq) in polymer {
+        if let Some(ch) = transforms.get(&pair) {
+            *new_polymer.entry((pair.0, *ch as u8)).or_insert(0) += freq;
+            *new_polymer.entry((*ch as u8, pair.1)).or_insert(0) += freq;
+        } else {
+            new_polymer.insert(pair, freq);
+        }
+    }
+    new_polymer
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
+
+    #[test]
+    fn sample1() {
+        assert_eq!(part1(TEST_INPUT), 1588);
+    }
+    #[test]
+    fn sample2() {
+        assert_eq!(part2(TEST_INPUT), 2188189693529);
+    }
+}
